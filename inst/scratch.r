40d4f9884d8675efbd948a9db0ce742730696cac
Scott-Zuo <scott.zuo@yale.edu>
2022-11-08 11:26:59 -0500

    Initial commit.

---
+++ inst/scratch.r
@@ -0,0 +1,53 @@
+library(devtools)
+library(lubridate)
+library(dplyr)
+library(ggplot2)
+library(tidyr)
+
+document()
+document()
+
+data(ukb_accel)
+
+dt = first(ukb_accel$time)
+
+ukb_accel |>
+  filter(time >= dt  & time < dt + minutes(5)) |>
+#  (\(x) x[seq(1, nrow(x), by =100),])() |>
+  accel_plot() +
+    theme_minimal() +
+    xlab("Time") +
+    ylab("Acceleration (in gravities)")
+
+ukb_accel |>
+  filter(time >= dt & time < dt + minutes(5) ) |>
+  spectral_signature(take_log = TRUE) |>
+ # filter(freq <= 10) |>
+  accel_plot()
+
+#check()
+
+#R CMD check
+
+#library(usethis)
+#use_testthat()
+#use_test("plot")
+#test()
+
+#library(covr)
+#covr::report()
+
+#git init
+#then terminate R
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ tests/testthat/test-plot.R
@@ -0,0 +1,51 @@
+
+
+
+
+test_that(
+  "The accel_plot() returns a ggplot object.",
+  {
+    data(ukb_accel)
+    p =  accel_plot(ukb_accel[1:100,])
+    expect_true(inherits(p, "gg"))
+  }
+)
+
+test_that(
+  "The accel_plot() errors when no time or freq column.",
+  {
+    data(iris)
+    expect_error(accel_plot(iris))
+  }
+)
+
+test_that(
+  "The accel_plot() is correct for time-series data.",
+  {
+    data(ukb_accel)
+    p = accel_plot(ukb_accel[1:100,])
+    expect_doppelganger("first-100-samples", p)
+  }
+)
+
+#doppelganger: first time, generate the svg figure; then compare subject to
+#the figure generated before
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
